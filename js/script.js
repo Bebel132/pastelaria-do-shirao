685d46c8ad6bb973658c6cfcd7f5962b9bc565ad
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2022-01-31 22:05:00 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 AOS.init();
+
+let menu_hidden = document.querySelector('.menu')
+
+let bars = document.querySelector('.fa-bars').addEventListener('click', () => {
+    menu_hidden.classList.toggle('menu-block');
+});
